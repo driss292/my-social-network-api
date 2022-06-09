9dca30d70c2de61f8a7c867ae7ab4c590382c5d2
driss292 <drisskaci@outlook.fr>
2022-06-09 13:45:10 +0200

    config-user-route/model

---
+++ models/user.model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+// const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    pseudo: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLegth: 55,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minLength: 6,
+    },
+    picture: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: [String],
+    },
+    followers: {
+      type: [String],
+    },
+    following: {
+      type: [String],
+    },
+    likes: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const express = require("express");
 const cors = require("cors");
 const colors = require("colors");
 
-// Connexion à la DB
 require("./config/db");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Import Routes
+const userRoutes = require("./routes/user.routes");
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.json("Bienvenue sur My-Social-Network-API");
 });
 
+app.get("*", (req, res) => {
+  res.status(404).json("Page introuvable");
+});
+
+// Server
 app.listen(process.env.PORT, () => {
   console.log(colors.america(`server OK on ${process.env.PORT}`));
 });
